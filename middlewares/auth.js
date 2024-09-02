ef2a2f1929c82db50169e42d51fd73f8e28ee0e8
Kenneth Ekandem <kennyekandem@gmail.com>
2024-09-03 00:45:08 +0100

    user verification middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
         return res.status(403).json({ message: 'User Unauthorized! Please Open an Account!' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
+    try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
         req.user = { id: decoded.userId }; // Attach user object with userId to the request
         next();
-    });
+    } catch ( error ) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 };
 
 module.exports = {
--- routes/hotel.route.js
+++ routes/hotel.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { lists, searchLocation, searchHotels, book } = require('../controllers/hotelController');
+const auth = require('../middlewares/auth');
 
 // hotels APIs
 router.get('/hotels/search-location/:search', searchLocation)
 router.get('/hotels/listHotels', searchHotels)
-router.post('/hotels/book', book)
+router.post('/hotels/book', [auth.verifyToken], book)
 
 module.exports = router;
