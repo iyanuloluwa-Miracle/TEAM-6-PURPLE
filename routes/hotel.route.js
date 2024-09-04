ef7e70880b17125bf0a9bb069fd4c3dd7f59a811
Kenneth Ekandem <kennyekandem@gmail.com>
2024-09-04 11:29:14 +0100

    update to hotels route

--- routes/hotel.route.js
+++ routes/hotel.route.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const { lists, searchLocation, searchHotels, book, listAttractions, searchAttractionLocation, deleteBooking } = require('../controllers/hotelController');
 const auth = require('../middlewares/auth');
+const { methodNotAllowedHandler } = require("../middlewares/errorHandler");
 
 // hotels APIs
-router.get('/hotels/search-location/:search', searchLocation);
-router.get('/hotels/listHotels', searchHotels);
-router.post('/hotels/book', [auth.verifyToken], book);
-router.delete('/hotels/:id', deleteBooking)
+router.route('/hotels/search-location/:search')
+    .get(searchLocation)
+    .all(methodNotAllowedHandler);
+router.route('/hotels/listHotels')
+    .get(searchHotels)
+    .all(methodNotAllowedHandler);
+router.route('/hotels/book')
+    .post([auth.verifyToken], book)
+    .all(methodNotAllowedHandler);
+router.route('/hotels/:id')
+    .delete(deleteBooking)
 
-router.get('/attractions/:search', searchAttractionLocation);
-router.get('/attractions/list', listAttractions)
+// attractions APIs
+router.route('/attractions/:search')
+    .get(searchAttractionLocation)
+    .all(methodNotAllowedHandler);
+router.route('/attractions/list')
+    .get(listAttractions)
+    .all(methodNotAllowedHandler);
+
+    
 module.exports = router;
